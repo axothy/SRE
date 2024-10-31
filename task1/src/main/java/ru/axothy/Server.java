6e341d85ecd0b02fdbc5a28a1668a7635f1e3714
Чеботин Александр <Chebotin.A@sbertech.ru>
2024-10-31 16:39:48 +0300

    test

--- task1/src/main/java/ru/axothy/Server.java
+++ task1/src/main/java/ru/axothy/Server.java
@@ -25,6 +25,7 @@ public class Server {
         public void handle(HttpExchange exchange) throws IOException {
             String response = "Ok";
 
+            //test
             exchange.sendResponseHeaders(200, response.length());
             OutputStream os = exchange.getResponseBody();
             os.write(response.getBytes());
